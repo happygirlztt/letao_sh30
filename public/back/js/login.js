95c14e0efee068ff469dbe85e836bdab0a601c5d
jepson <jepsonpp@qq.com>
2018-11-21 15:50:29 +0800

    登录功能提交校验成功

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -28,6 +28,9 @@ $(function() {
             min: 2,
             max: 6,
             message: "用户名长度必须是2-6位"
+          },
+          callback: {
+            message: "用户名错误"
           }
         }
       },
@@ -44,6 +47,10 @@ $(function() {
             min: 6,
             max: 12,
             message: "密码长度必须是6-12位"
+          },
+          //专门用于 ajax 回调提示的说明
+          callback: {
+            message: "密码错误"
           }
         }
       }
@@ -67,10 +74,18 @@ $(function() {
           location.href("index.html");
         }
         if (info.error === 1000) {
-          alert(info.message);
+          // 用户名不存在
+          // 参数1：字段名称
+          // 参数2：校验状态
+          // 参数3：配置规则，用于提示
+          $("#form")
+            .data("bootstrapValidator")
+            .updateStatus("username", "INVALID", "callback");
         }
         if (info.error === 1001) {
-          alert(info.message);
+          $("#form")
+            .data("bootstrapValidator")
+            .updateStatus("password", "INVALID", "callback");
         }
       }
     });
