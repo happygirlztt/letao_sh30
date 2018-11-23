07fc7259670a0e568ead3d5abf316e5267b69697
jepson <jepsonpp@qq.com>
2018-11-24 01:07:28 +0800

    二级分类-上传，隐藏域，表单校验完成

--- public/back2/js/second.js
+++ public/back2/js/second.js
@@ -34,4 +34,106 @@ $(function() {
       }
     });
   }
+
+  //2.点击添加分类按钮，显示模态框
+  $("#addBtn").click(function() {
+    $("#addModal").modal("show");
+
+    // 发送 Ajax 请求，请求所有的一级分类列表,进行渲染
+    $.ajax({
+      type: "get",
+      url: "/category/queryTopCategoryPaging",
+      data: {
+        page: 1,
+        pageSize: 100
+      },
+      dataType: "json",
+      success: function(info) {
+        console.log(info);
+        var htmlStr = template("dropdownTpl", info);
+        $(".dropdown-menu").html(htmlStr);
+      }
+    });
+  });
+
+  //3.给下拉菜单添加选中功能(事件委托)
+  $(".dropdown-menu").on("click", "a", function() {
+    // 获取 a 文本
+    var txt = $(this).text();
+    // console.log(txt);
+
+    // 设置给按钮里的 span
+    $("#dropdownText").text(txt);
+
+    // 获取 a 中自定义属性储存的 id
+    var id = $(this).data("id");
+    // 赋值给隐藏域，用于提交
+    $('[name="categoryId"]').val(id);
+
+    //手动将隐藏域的校验状态，改成成功
+    //updateStatus
+    // 参数1 字段名称
+    // 参数2 校验状态 VALID成功
+    // 参数3 配置校验规则，用来配置错误提示信息
+    $("#form")
+      .data("bootstrapValidator")
+      .updateStatus("categoryId", "VALID");
+  });
+
+  // 4.调用 fileUpload 方法，发送文件上传请求
+  $("#fileupload").fileupload({
+    dataType: "json", //返回回来的数据类型
+    done: function(e, data) {
+      console.log(data);
+      var result = data.result; //后台返回的结果
+      // 获取图片地址，赋值给img的src
+      var picUrl = result.picAddr;
+      $("#imgBox img").attr("src", picUrl);
+
+      // 将图片地址赋值给隐藏域
+      $('[name="brandLogo"]').val(picUrl);
+      //重置隐藏域的校验状态
+      $("#form")
+        .data("bootstrapValidator")
+        .updateStatus("brandLogo", "VALID");
+    }
+  });
+
+  // 5.添加表单校验
+  $("#form").bootstrapValidator({
+    //配置排除项，需要对隐藏域进行校验
+    excluded: [],
+    // 配置小图标
+
+    feedbackIcons: {
+      valid: "glyphicon glyphicon-ok", // 校验成功
+      invalid: "glyphicon glyphicon-remove", // 校验失败
+      validating: "glyphicon glyphicon-refresh" // 校验中
+    },
+
+    // 配置校验字段
+    fields: {
+      categoryId: {
+        validators: {
+          notEmpty: {
+            message: "请输入一级分类"
+          }
+        }
+      },
+      brandName: {
+        validators: {
+          notEmpty: {
+            message: "请输入二级分类"
+          }
+        }
+      },
+      brandLogo: {
+        validators: {
+          notEmpty: {
+            message: "请上传图片"
+          }
+        }
+      }
+    }
+  });
 });
